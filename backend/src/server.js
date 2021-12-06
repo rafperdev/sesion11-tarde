1c947df89f1880efec73af629f31a3089b498cf5
Rafael Perez <rbperez@uninorte.edu.co>
2021-12-06 14:53:47 -0500

    Conexion con MongoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,35 @@
 const express = require("express");
 const cors = require("cors");
-const { productos } = require("./datos");
+const mongoose = require("mongoose");
+const Productos = require("./modelos/productosModel");
 const app = express();
 
 app.use(cors()) //Middleware cors
 app.use(express.json()); //Middleware para convertir a JSON
+//Conecta a la BD
+mongoose.connect("mongodb://127.0.0.1:27017/comercio")
+    .then(res => console.log("Conectado a BD"))
+    .catch(error => console.log(error));
+
 // API, endpoint, rutas, API Rest
 app.get("/", function (req, res) {
     res.send("Bienvenidos a la clase!!");
 });
 
 //API Rest: verbo, ruta, endpoint
-app.get("/producto/consultar/:name", function (req, res) {
-    const name = req.params.name;
-    const prod = productos.find(p => p.title.toLowerCase() === name.toLowerCase());
-    res.send(prod);
+app.post("/producto/consultar", function (req, res) {
+    const { nombre } = req.body // {nombre:"mango",precio:200,stock:30}
+    Productos.findOne({ nombre }, function (error, prod) {
+        if (error) {
+            return res.send({ estado: "error", msg: "ERROR al buscar Producto" })
+        } else {
+            if (prod !== null) {
+                res.send({ estado: "ok", msg: "Producto Encontrado", data: prod });
+            } else {
+                res.send({ estado: "error", msg: "Producto NO Encontrado" });
+            }
+        }
+    })
 })
 
 /**
@@ -25,17 +40,17 @@ app.get("/producto/consultar/:name", function (req, res) {
  * Datos de entrada: {nombre:"mango",precio:200,stock:30}
  * Respuesta: {estado: "ok", msg: "Producto Guardado!"}
  */
-app.post("/producto/guardar", (req, res) => {
-    // Capturar los datos que vienen del cliente
-    const { nombre, precio, stock } = req.body;
-    // Crear un JSON con los datos capturados
-    const prod = { title: nombre, price: precio, stock };
-    // Guardarlos en el array de Productos
-    productos.push(prod);
-    console.log(productos);
-    // Responder al cliente
-    res.send({ estado: "ok", msg: "Producto Guardado!" });
-})
+app.post("/producto/guardar", function (req, res) {
+    const data = req.body;
+    const prod = new Productos(data);
+    prod.save(function (error) {
+        if (error) {
+            res.send({ estado: "error", msg: "ERROR: Producto NO Guardado :(" });
+            return false;
+        }
+        res.send({ estado: "ok", msg: "Producto Guardado!" });
+    })
+});
 
 /**
  * API Rest Editar Producto
@@ -78,7 +93,7 @@ app.post("/producto/eliminar", (req, res) => {
     let i = 0;
     for (const p of productos) {
         if (p.title.toLowerCase() == nombre.toLowerCase()) {
-            productos.splice(i,1);
+            productos.splice(i, 1);
             break;
         }
         i++;
